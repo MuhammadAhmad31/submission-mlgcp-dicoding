b0d101bd2cfc9bd9a8d7ad3f112ffb24e19558ea
MuhammadAhmad31 <ahmadsilva7@gmail.com>
2024-12-08 01:27:52 +0700

    fix: bug on inferenceService

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -3,16 +3,16 @@ const InputError = require('../exceptions/InputError');
  
 async function predictClassification(model, image) {
     try {
-        const prediction = model.predict(tensor);
-        const score = await prediction.data();
-        const confidenceScore = Math.max(...score) * 100;
- 
         const tensor = tf.node
             .decodeJpeg(image)
             .resizeNearestNeighbor([224, 224])
             .expandDims()
             .toFloat()
   
+        const prediction = model.predict(tensor);
+        const score = await prediction.data();
+        const confidenceScore = Math.max(...score) * 100;
+ 
         const label = confidenceScore <= 50 ? 'Non-cancer' : 'Cancer'; 
         let suggestion;
  
